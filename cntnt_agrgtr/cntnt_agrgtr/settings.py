c054e8f4d403da8e054bd2538d1c13d5cac86c92
Илья Королев <ilya_14_2005@mail.ru>
2020-10-15 23:43:59 +0300

    hello

--- cntnt_agrgtr/cntnt_agrgtr/settings.py
+++ cntnt_agrgtr/cntnt_agrgtr/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'get_cntnt.apps.GetCntntConfig'
+    'get_cntnt.apps.GetCntntConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- cntnt_agrgtr/get_cntnt/models.py
+++ cntnt_agrgtr/get_cntnt/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Source(models.Model):
     name = models.CharField(max_length=200)
     url = models.URLField(max_length=200)
+    def __str__(self):
+        return self.name
 
 class News(models.Model):
     source = models.ForeignKey(Source, on_delete = models.CASCADE)
     news_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.news_text
