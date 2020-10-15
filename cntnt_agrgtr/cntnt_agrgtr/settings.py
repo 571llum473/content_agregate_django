86a2caf1b5388525bf775cbdb37ab6e71939033d
Илья Королев <ilya_14_2005@mail.ru>
2020-10-15 20:50:55 +0300

    hello

--- cntnt_agrgtr/cntnt_agrgtr/settings.py
+++ cntnt_agrgtr/cntnt_agrgtr/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'get_cntnt.apps.GetCntntConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
