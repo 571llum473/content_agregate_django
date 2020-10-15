a938a0a2168f31229ad07086604328b71f04c808
Mars <forb0re4l1s@yandex.ru>
2020-10-15 21:18:59 +0300

    adding_models

--- cntnt_agrgtr/get_cntnt/models.py
+++ cntnt_agrgtr/get_cntnt/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Source(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+
+class News(models.Model):
+    source = models.ForeignKey(Source, on_delete = models.CASCADE)
+    news_text = models.CharField(max_length=200)
