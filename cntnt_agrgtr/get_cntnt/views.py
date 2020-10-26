4145b975a15257022052b0309faca9fbe09e691b
Mars <forb0re4l1s@yandex.ru>
2020-10-26 21:56:40 +0300

    1st release

--- cntnt_agrgtr/get_cntnt/views.py
+++ cntnt_agrgtr/get_cntnt/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 from .models import Source
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    sources = Source.objects.all()
+    template = loader.get_template('get_cntnt/index.html')
+    latest_news = {i : i.news_set.order_by('-pub_time')[:5] for i in sources}
+    context = {
+        'latest_news' : latest_news,
+    }
+    return HttpResponse(template.render(context, request))
