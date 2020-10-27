129c397ddecc2676e1ea7128d5f04a5a26267c3e
Mars <forb0re4l1s@yandex.ru>
2020-10-27 20:56:42 +0300

    1

--- cntnt_agrgtr/get_cntnt/models.py
+++ cntnt_agrgtr/get_cntnt/models.py
@@ -5,7 +5,23 @@ from django.utils import timezone
 class Source(models.Model):
     name = models.CharField(max_length=200)
     url = models.URLField(max_length=200)
-    category = 
+    TECH = 'tech'
+    SPORT = 'sport'
+    POLITICS = 'politics'
+    ENTERTAINMENT = 'entertainment'
+    LIFESTYLE = 'lifestyle'
+    BUSINESS = 'business'
+    SCIENCE = 'science'
+    CAT_CHOICES = [
+        (TECH, 'Технологии'),
+        (SPORT, 'Спорт'),
+        (POLITICS, 'Политика'),
+        (ENTERTAINMENT, 'Развлечения'),
+        (LIFESTYLE, 'Образ жизни'),
+        (BUSINESS, 'Бизнес'),
+        (SCIENCE, 'Наука')
+    ]
+    category = models.CharField(max_length=20, choices = CAT_CHOICES, default = LIFESTYLE)
     def __str__(self):
         return self.name
 
@@ -13,7 +29,7 @@ class News(models.Model):
     source = models.ForeignKey(Source, on_delete = models.CASCADE)
     news_text = models.CharField(max_length=200)
     pub_time = models.TimeField('time published',  default=timezone.now)
-    snippet = models.CharField(max_length=200)
+    snippet = models.CharField(max_length=200, default='Новость недоступна для просмотра')
     url = models.URLField(max_length=200, unique=True)
     def __str__(self):
         return self.news_text
