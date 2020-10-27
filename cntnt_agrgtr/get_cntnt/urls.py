2afa6e460d98661d6983ccc42387cb76676ea074
Mars <forb0re4l1s@yandex.ru>
2020-10-27 22:07:32 +0300

    '2'

--- cntnt_agrgtr/get_cntnt/urls.py
+++ cntnt_agrgtr/get_cntnt/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-
+app_name = "get_cntnt"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:cat>', views.cat, name = 'cat')
 ]
--- cntnt_agrgtr/get_cntnt/views.py
+++ cntnt_agrgtr/get_cntnt/views.py
@@ -8,7 +8,9 @@ def index(request):
     sources = Source.objects.all()
     template = loader.get_template('get_cntnt/index.html')
     latest_news = {i : i.news_set.order_by('-pub_time')[:5] for i in sources}
+    cat_choices = {i[0] : i[1] for i in Source.CAT_CHOICES}
     context = {
         'latest_news' : latest_news,
+        'CAT_CHOICES' : cat_choices
     }
     return HttpResponse(template.render(context, request))
