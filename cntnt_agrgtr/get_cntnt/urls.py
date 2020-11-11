6670b938a2c2fbc2782033f3678fb8996348b052
Илья Королев <ilya_14_2005@mail.ru>
2020-11-12 00:53:34 +0300

    reg

--- cntnt_agrgtr/get_cntnt/urls.py
+++ cntnt_agrgtr/get_cntnt/urls.py
@@ -5,6 +5,7 @@ app_name = "get_cntnt"
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:cat>', views.index, name = 'cat'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login')
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout')
 ]
--- cntnt_agrgtr/get_cntnt/views.py
+++ cntnt_agrgtr/get_cntnt/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.template import loader
 from .models import Source
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 
 # Create your views here.
 def index(request, cat=None):
@@ -19,15 +21,41 @@ def index(request, cat=None):
     }
     return render(request, 'get_cntnt/index.html', context)
 
-def register(request):
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('get_cntnt:index')
     form = RegisterForm()
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-    context = {"form":form}
-    return render(request, 'registration/register.html', context)
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('get_cntnt:index')
 
-def login(request):
+    context = {"form":form}
+    return render(request, 'get_cntnt/register.html', context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('get_cntnt:index')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('get_cntnt:index')
+        else:
+            messages.error(request,'Не правильный логин или пароль')
+
     context = {}
-    return render(request, "registration/login.html", context)
+    return render(request, "get_cntnt/login.html", context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('get_cntnt:index')
