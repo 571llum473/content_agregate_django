ccdc6ccb538499e2b64252d99e8d93ef396e5ec9
Илья Королев <ilya_14_2005@mail.ru>
2020-11-16 20:27:28 +0300

    asdfa

--- cntnt_agrgtr/get_cntnt/admin.py
+++ cntnt_agrgtr/get_cntnt/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Source
+from .models import Source, Profile
 
 admin.site.register(Source)
+admin.site.register(Profile)
